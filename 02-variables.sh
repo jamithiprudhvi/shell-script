9ec3dade3a6aad68431aea6ced08d6521e3e0f9f
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-16 16:17:11 +0530

    variables changed

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-PERSION1=Ram
-PERSION2=Sameer
-
-echo "$PERSION1: Hello $PERSION2 good morning"
-echo "$PERSION2: hi $PERSION1 good morning"
-echo "$PERSION1: how are you s$PERSION2?"
-echo "$PERSION2: iam good $PERSION1. How are you?"
+03-variables.sh
---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1: Hello $PERSION2 good morning"
+echo "$PERSION2: hi $PERSION1 good morning"
+echo "$PERSION1: how are you s$PERSION2?"
+echo "$PERSION2: iam good $PERSION1. How are you?"
