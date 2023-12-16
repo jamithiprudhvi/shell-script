ef55664a8011c22eb7f0aad21624873b46b63601
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-16 15:16:21 +0530

    variables edited

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh: Hello suresh good morning"
-echo "Suresh: hi ramesh good morning"
-echo "ramesh: how are you suresh?"
-echo "suresh: iam good ramesh. How are you?"
+PERSION1=Ramesh
+PERSION2=Suresh
+
+echo "$PERSION1: Hello $PERSION2 good morning"
+echo "$PERSION2: hi $PERSION1 good morning"
+echo "$PERSION1: how are you s$PERSION2?"
+echo "$PERSION2: iam good $PERSION1. How are you?"
