6263c2309a0d93aaa0aeea43f693a77014a69d49
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-18 23:39:04 +0530

    loops

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -5,6 +5,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2.... $R FAILED $N"
+    else 
+        echo "$2.... $R SUCCESS $N"
+}
+
 if [ $ID -ne 0 ]
 then
     echo "$R you are not a root user $N"
@@ -13,4 +21,13 @@ else
     echo "you are a root user"
 fi
 
-echo "All the arguments passed: $@"
+#echo "All the arguments passed: $@"
+
+for package in $@
+do
+
+yum install $package -y
+
+VALIDATE $? "Instillation $package"
+
+done
