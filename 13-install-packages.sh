695ee463dcad15fb9ef4e00290bde6f2378c4c53
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-18 23:43:22 +0530

    loops

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -9,8 +9,9 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e "$2.... $R FAILED $N"
+        exit 1
     else 
-        echo -e "$2.... $R SUCCESS $N"
+        echo -e "$2.... $G SUCCESS $N"
     fi
 }
 
