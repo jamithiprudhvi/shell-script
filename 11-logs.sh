749eb94a1c662f118c57475abc2c4fc6ea84d054
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-18 19:22:53 +0530

    logs

--- 11-logs.sh
+++ 11-logs.sh
@@ -8,7 +8,7 @@ echo "script name: $0"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo  -e "$2.... \e[31m FAILED"
+        echo -e "$2.... \e[31m FAILED"
         exit 1
     else
         echo -e "$2.... \e[32m SUCCESS"
