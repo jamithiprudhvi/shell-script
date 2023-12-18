703e5db7cd7d56c79b64cc704b5e8e242464790f
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-18 23:17:41 +0530

    loops

---
+++ 13-install-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ID=[ id -u ]
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $ID -ne 0 ]
+then
+    echo "$R you are not a root user $N"
+    exit 1
+else
+    echo "you are a root user"
+fi
+
+echo "All the arguments passed: $@"
