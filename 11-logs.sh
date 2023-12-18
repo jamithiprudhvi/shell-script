bcd8ba315e1ce817799d24fbdb7c5a86c61c2cd2
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-18 13:15:18 +0530

    ADDED FRUITS

---
+++ 11-logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ID=$(id -u) 
+echo "script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2....FAILED"
+        exit 1
+    else
+        echo "$2....SUCCESS"
+    fi
+}
+
+if [ $id -ne 0 ]
+then  
+    echo "you are not a root user"
+    exit 1
+else
+    echo " you are a root user"
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing GIT"
+
+
+
+
