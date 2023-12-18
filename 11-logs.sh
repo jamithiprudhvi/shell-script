57b0dec9c6ad16543cf18a2f3ba9f88a9d9dffa3
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-18 22:15:10 +0530

    logs

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
 ID=$(id -u) 
+
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 R="\e[31m"
-G="\e[32m
+
+G="\e[32m"
+
 Y="\e[33m"
+
 N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
@@ -34,4 +39,4 @@ VALIDATE $? "Installing mysql"
 
 yum install git -y &>> $LOGFILE
 
-VALIDATE $? "Installing GIT" 
+VALIDATE $? "Installing GIT"
