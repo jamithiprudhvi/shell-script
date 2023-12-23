e4651814e94ff86b41e9c95a707351f9a4a76409
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-23 23:57:43 +0530

    greeting script

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+NAME=""
+WISHES=""
+
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes Example: Good morning"
+    echo " -h, Display Help and exist" 
+}
+
+while getopts ":w:n:h:" opt; do
+    case $opt in
+        n) NAME="OPTARG";;
+        w) WISHES="OPTARG";;
+        h|*) USAGE; exit;;
+    esac
+
+
+done
