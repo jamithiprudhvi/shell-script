e0b37fddd2ba14b8be1aac109cf45dc70e3dd1e8
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-01-01 15:08:25 +0530

    script-practice

--- shell-script-practice/04-arrays.sh
+++ shell-script-practice/04-arrays.sh
@@ -2,6 +2,6 @@
 
 FRUITS=("apple" "mango" "banana")
 
-echo "first fruit is : $FRUITS[0]"
-echo "second fruit is : $FRUITS[1]"
-echo "Third fruit is : $FRUITS[2]"
+echo "first fruit is : ${FRUITS[0]}"
+echo "second fruit is : ${FRUITS[1]}"
+echo "Third fruit is : ${FRUITS[2]}"
