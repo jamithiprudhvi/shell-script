a1d9f1ad860787fc6050fe94f2a661fd3e8b4b39
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-01-01 16:25:39 +0530

    script-practice

--- shell-script-practice/06-Install-mysql.sh
+++ shell-script-practice/06-Install-mysql.sh
@@ -2,11 +2,12 @@
 
 ID=$(id -u)
 
-if [ $ID -ge 0 ]
+if [ $ID -ne 0 ]
 then
-    echo "you are not a root user"
+    echo "you are a not root user"
+    exit 1
 else
     echo "you are a root user"
 fi
 
-yum install mysql -y
+
