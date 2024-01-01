145b3cd3d6b5a968713155058605966a8d589004
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-01-01 09:59:45 +0530

    script-practice

---
+++ shell-script-practice/02-shell-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your user name"
+read USERNAME
+echo "please enter your password"
+read -s PASSWORD
+
+echo "your USERNAME and PASSWORD IS $USERNAME and $PASSWORD
+
